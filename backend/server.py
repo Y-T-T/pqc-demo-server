721007c6845097efc10b16c063fa7bdec5694087
Y-T-T <timu.tsai@gmail.com>
2024-01-26 21:17:54 +0800

    Update UI & Server POST method

--- backend/server.py
+++ backend/server.py
@@ -4,29 +4,32 @@ from flask import Flask, request, send_from_directory
 app = Flask(__name__, static_folder='../frontend/build')
 
 @app.route('/', defaults={'path':''})
-@app.route('/<path:path>', methods=['GET', 'POST'])
+@app.route('/<path:path>')
 def index(path):
-    if request.method == 'POST':
-        data = request.form
-        return "POST request received with data: " + str(data) + '\n'
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
     else:
-        # print(path)
-        if path != "" and os.path.exists(app.static_folder + '/' + path):
-            return send_from_directory(app.static_folder, path)
-        else:
-            return send_from_directory(app.static_folder, 'index.html')
-        # return 'GET request received\n'
+        return send_from_directory(app.static_folder, 'index.html')
+    # return 'GET request received\n'
+
+@app.route('/handle_msg', methods=['POST'])
+def post_test():
+    if request.method == 'POST':
+        # print(request.json)
+        data = request.json
+        print(data)
+        return {"response": str(request.remote_addr) + " sends: " + str(data['msg']) + '\n'}
 
 @app.route('/data')
 def get_time():
     x = datetime.datetime.now() 
     # Returning an api for showing in  reactjs
     return {
-        'Name':"geek", 
+        'Name':"geek",
         "Age":"22",
         "Date":x, 
         "programming":"python"
         }
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, use_reloader=True, threaded=True)
+    app.run(host='127.0.0.1', port=8080, use_reloader=True, threaded=True, debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,197 @@
 // Filename - App.js
 
 // Importing modules
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-	// usestate for setting a javascript
-	// object for storing and using data
-	const [data, setdata] = useState({
-		name: "",
-		age: 0,
-		date: "",
-		programming: "",
-	});
 
-	// Using useEffect for single rendering
+	const [msgs, setmsgs] = useState([]);
+	const endOfMessagesRef = useRef(null);
+
+	const addNewMsg = (msg) => {
+		if(msgs.length > 0){
+			setmsgs([...msgs.slice(0, -1), msg, <br/>]);
+		}
+		else{
+			setmsgs([...msgs, msg, <br/>]);
+		}
+	};
 	useEffect(() => {
-		// Using fetch to fetch the api from 
-		// flask server it will be redirected to proxy
-		fetch("/data").then((res) =>
-			res.json().then((data) => {
-				// Setting a data from api
-				setdata({
-					name: data.Name,
-					age: data.Age,
-					date: data.Date,
-					programming: data.programming,
-				});
-			})
-		);
-	}, []);
+        console.log("msgs:", msgs);
+    }, [msgs]);
 
+	useEffect(() => {
+        if (endOfMessagesRef.current) {
+            endOfMessagesRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [msgs]);
+
+	// // Using useEffect for single rendering
+	// useEffect(() => {
+	// 	// Using fetch to fetch the api from 
+	// 	// flask server it will be redirected to proxy
+		// fetch("/data").then((res) =>
+		// 	res.json().then((data) => {
+		// 		// Setting a data from api
+		// 		setdata({
+		// 			name: data.Name,
+		// 			age: data.Age,
+		// 			date: data.Date,
+		// 			programming: data.programming,
+		// 		});
+		// 		console.log(data);
+		// 	})
+		// );
+	// }, []);
+
+	const [inputValue, setInputValue] = useState('');
+    const [cursorPosition, setCursorPosition] = useState(0);
+
+    useEffect(() => {
+        setCursorPosition(getTextWidth(inputValue));
+    }, [inputValue]);
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const getTextWidth = (text) => {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        context.font = '16px monospace';
+        return context.measureText(text).width;
+    };
+	
+	const inputRef = useRef(null);
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            // 如果當前焦點已在 input 內，則不進行操作
+            if (document.activeElement === inputRef.current) {
+                return;
+            }
+
+            // 將焦點設定到 input 上
+            inputRef.current.focus();
+
+            // 防止在輸入時觸發其他鍵盤快捷鍵
+            event.preventDefault();
+        };
+
+        // 添加事件監聽器
+        document.addEventListener('keydown', handleKeyDown);
+
+        // 清除事件監聽器
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+
+	const handleSubmit = async (event) => {
+        event.preventDefault();
+		// try {
+        //     const response = await fetch('/data', {
+        //         method: 'GET',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //         // body: JSON.stringify({ data: inputValue }),
+        //     });
+
+        //     if (!response.ok) {
+        //         throw new Error('Network response was not ok');
+        //     }
+
+        //     await response.json().then((data) => {
+		// 			// Setting a data from api
+		// 			setdata({
+		// 				name: data.Name,
+		// 				age: data.Age,
+		// 				date: data.Date,
+		// 				programming: data.programming,
+		// 			});
+		// 		});
+		// 	// console.log(responseData);
+        //     // setResponse(responseData);
+			
+        //     setInputValue('');
+        // } catch (error) {
+        //     console.error('Error:', error);
+        // }
+		try {
+            const response = await fetch('/handle_msg', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ msg: inputValue }),
+            });
+			
+			// console.log(JSON.stringify({ data: inputValue }));
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const responseData = await response.json();
+			console.log(responseData);
+		
+			addNewMsg(responseData.response);
+
+            setInputValue('');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+	
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h2>PQC demo</h2>
 			</header>
 			<center>
-				{/* <div>
-					<p>{data.name}</p>
-					<p>{data.age}</p>
-					<p>{data.date}</p>
-					<p>{data.programming}</p>
-				</div> */}
 				<div className="content-Box">
 					<div className="send-Box">
 						<h2>Send</h2>
-						<div class="fakeMenu">
-							<div class="fakeButtons fakeClose"></div>
-							<div class="fakeButtons fakeMinimize"></div>
-							<div class="fakeButtons fakeZoom"></div>
+						<div className="fakeMenu">
+							<div className="fakeButtons fakeClose"></div>
+							<div className="fakeButtons fakeMinimize"></div>
+							<div className="fakeButtons fakeZoom"></div>
 						</div>
-						<div class="fakeScreen">
-							<form method="POST">
-								<label>
-									<p>Send:</p>
-									<input type="text" name="input" />
-								</label>
-								&emsp;
-								<input type="submit" value="Submit" />
-							</form>
-							{/* <p class="line4"><span class="cursor4">_</span></p> */}
+						<div className="fakeScreen">
+							<div className="form-container">
+								<form onSubmit={handleSubmit}>
+									<label>
+										<div className="flex-container">
+											<div>
+												<span>&gt;&nbsp;</span>
+											</div>
+											<div className="inputBorder">
+											<input className="msgInput" type="text" ref={inputRef} value={inputValue} onChange={handleInputChange} autoComplete="off" name="input" />
+											<span className="cursor" style={{left: cursorPosition + 2}}>_</span>
+											</div>
+										</div>
+									</label>
+									&emsp;
+									<input type="submit" value="Send" style={{display: 'none'}} />
+								</form>
+							</div>
 						</div>
 					</div>
-					<div className="recieve-Box">
-						<h2>Recieve</h2>
-						<div class="fakeMenu">
-							<div class="fakeButtons fakeClose"></div>
-							<div class="fakeButtons fakeMinimize"></div>
-							<div class="fakeButtons fakeZoom"></div>
+					<div className="log-Box">
+						<h2>Log</h2>
+						<div className="fakeMenu">
+							<div className="fakeButtons fakeClose"></div>
+							<div className="fakeButtons fakeMinimize"></div>
+							<div className="fakeButtons fakeZoom"></div>
 						</div>
-						<div class="fakeScreen">
-							{/* <p class="line4"><span class="cursor4">_</span></p> */}
+						<div className="fakeScreen logScreen">
+							<div className="log">
+								{msgs.map((msg, index) => (
+									<p key={index}>{msg}</p>
+								))}
+								<div ref={endOfMessagesRef}></div>
+							</div>
 						</div>
 					</div>
 				</div>
