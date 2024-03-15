930f62e4c32547f8849deddb35b7e03464fe25fc
Y-T-T <timu.tsai@gmail.com>
2024-03-15 23:26:21 +0800

    close debug mode

--- backend/tls/handshake.c
+++ backend/tls/handshake.c
@@ -18,38 +18,38 @@ void parse_client_hello(u8 *buffer, ssize_t buffer_len, HANDSHAKE_HELLO_MSG_CTX 
     // Record Header
     len = 5;
     memcpy((*client_hello).record_header, &buffer[idx], len);
-    printf("Record Header:\n");
-    print_bytes((*client_hello).record_header, len);
+    // printf("Record Header:\n");
+    // print_bytes((*client_hello).record_header, len);
     idx += len;
 
     // Handshake Header
     len = 4;
     memcpy((*client_hello).handshake_header, &buffer[idx], len);
-    printf("Handshake Header:\n");
-    print_bytes((*client_hello).handshake_header, len);
+    // printf("Handshake Header:\n");
+    // print_bytes((*client_hello).handshake_header, len);
     idx += len;
 
     // Client Version
     len = 2;
     memcpy((*client_hello).tls_version, &buffer[idx], len);
-    printf("Client Version:\n");
-    print_bytes((*client_hello).tls_version, len);
+    // printf("Client Version:\n");
+    // print_bytes((*client_hello).tls_version, len);
     idx += len;
     
     // Client Random
     len = 32;
     memcpy((*client_hello).random, &buffer[idx], len);
-    printf("Client Random:\n");
-    print_bytes((*client_hello).random, len);
+    // printf("Client Random:\n");
+    // print_bytes((*client_hello).random, len);
     idx += len;
 
     // Session ID
     len = 33;
     memcpy((*client_hello).session_id.length, &buffer[idx], 1);
     memcpy((*client_hello).session_id.id, &buffer[idx+1], 32);
-    printf("Session ID:\n");
-    print_bytes((*client_hello).session_id.length, 1);
-    print_bytes((*client_hello).session_id.id, 32);
+    // printf("Session ID:\n");
+    // print_bytes((*client_hello).session_id.length, 1);
+    // print_bytes((*client_hello).session_id.id, 32);
     idx += len;
 
     // Cipher Suites
@@ -57,22 +57,22 @@ void parse_client_hello(u8 *buffer, ssize_t buffer_len, HANDSHAKE_HELLO_MSG_CTX 
     len += 2;
     client_hello->cipher_suites = malloc(len * sizeof(u8));
     memcpy(client_hello->cipher_suites, &buffer[idx], len);
-    printf("Cipher Suites:\n");
-    print_bytes(client_hello->cipher_suites, len);
+    // printf("Cipher Suites:\n");
+    // print_bytes(client_hello->cipher_suites, len);
     idx += len;
 
     // Compression Methods
     len = 2;
     memcpy((*client_hello).compression_method, &buffer[idx], len);
-    printf("Compression Methods:\n");
-    print_bytes((*client_hello).compression_method, len);
+    // printf("Compression Methods:\n");
+    // print_bytes((*client_hello).compression_method, len);
     idx += len;
 
     // Extensions len
     len = 2;
     memcpy((*client_hello).extensions_length, &buffer[idx], len);
-    printf("Extensions len:\n");
-    print_bytes((*client_hello).extensions_length, len);
+    // printf("Extensions len:\n");
+    // print_bytes((*client_hello).extensions_length, len);
     idx += len;
     eidx = idx;
 
